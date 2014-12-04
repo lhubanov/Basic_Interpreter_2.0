d037765a8197fd8d805e7f21e02d1f0140a4b2d1
yazoo123 <yazoo_123@hotmail.com>
2014-12-04 14:49:02 +0000

    Create ifclass.h

---
+++ ifclass.h
@@ -0,0 +1,16 @@
+#include "database.h"
+
+class ifclass{
+	
+	public:
+		
+		ifclass(STL_Map map, std::string data);
+		~ifclass(){};
+		int GetIteratorResult();
+		
+	private:
+		int IteratorResult;
+
+	
+	
+};
