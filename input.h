acb1e733efec9615741ac42568bb6964ccc2f0fe
yazoo123 <yazoo_123@hotmail.com>
2014-12-09 21:52:18 +0000

    Update input.h

--- input.h
+++ input.h
@@ -1,19 +1,27 @@
+//#ifndef input_H
+//#define input_H
+
+
 #include <iostream>
 //#include <fstream>
 //#include <sstream>
 #include <string>
 #include <map>
 #include <cstdlib>
+#include "node.h"
 
 
-class Input{
+class Input : public Node {
 	
 	public:
+	
 		Input(int temp_Inst, std::string temp_Func, std::string temp_Data);
 		
-		void Execute(std::map<int, Node*> m, std::map<std::string, int> v);
+		void Execute(std::map<int, Node*> m, std::map<std::string, int> &v, std::map<int, Node*>::iterator &i);
 		
 	private:
 	
 	
 };
+
+//#endif
