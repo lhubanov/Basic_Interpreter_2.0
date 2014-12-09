cf737379400bbd5423c4360cb3b2796f3a47baa8
yazoo123 <yazoo_123@hotmail.com>
2014-12-09 18:37:30 +0000

    print.h

---
+++ print.h
@@ -0,0 +1,30 @@
+#include "node.h"
+
+#include <iostream>
+//#include <fstream>
+//#include <sstream>
+#include <string>
+#include <map>
+#include <cstdlib>
+
+
+//class Node;
+
+class Print: public Node {
+	
+	public:
+		
+		Print(int temp_Inst, std::string temp_Func, std::string temp_Data);
+		
+		void Execute(std::map<int, Node*> m, std::map<std::string, int> v);
+	
+	protected:
+	
+	
+	private:
+	
+	
+		
+	
+};
+
