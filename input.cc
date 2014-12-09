91e672c0e5bab1aa030ca4ab011dd1249fd64537
yazoo123 <yazoo_123@hotmail.com>
2014-12-09 21:52:03 +0000

    Update input.cc

--- input.cc
+++ input.cc
@@ -1,30 +1,32 @@
-#inlude "input.h"
+#include "input.h"
 
 Input::Input(int temp_Inst, std::string temp_Func, std::string temp_Data):Node(temp_Inst, temp_Func, temp_Data) 
 {
 	std::cout << "Input Constructor- inst, func, data are: " << temp_Inst << temp_Func << temp_Data << std::endl;	
 }
 
-void Input::Execute(std::map<int, Node*> m, std::map<std::string, int> v)
+void Input::Execute(std::map<int, Node*> m, std::map<std::string, int> &v, std::map<int, Node*>::iterator &i )
 {
 	std::cout << "Input Execute called" << std::endl;		
 	
 	std::string temp;
-	
-	
 	std::cin >> temp;
+	Data.erase((Data.length()-1), 1);
 	
 	if (atoi(temp.c_str()) != 0) //if correct
 	{
 		//check if the (data) already exists..
-		int temp_val = atoi(temp.c_str())
-		v[temp_Data] = temp_val;
+		int temp_val = atoi(temp.c_str());
+		v[Data] = temp_val;
+		
+		std::cout << "I set v[Data] now" << std::endl;
+		std::cout << v[Data] << std::endl;
 	}
 	else
 	{
 		std::cout << "ERROR: INPUT HAS TO BE AN INTEGER" << std::endl;
 	}
 	
-	std::cout << Data << std::endl; 
+	//std::cout << Data << std::endl; 
 
 }
