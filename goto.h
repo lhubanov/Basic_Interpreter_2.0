63b0d5ca3b6b76195e52caacd07e8b5725f62d23
yazoo123 <yazoo_123@hotmail.com>
2014-12-09 21:53:40 +0000

    Create goto.h

---
+++ goto.h
@@ -0,0 +1,28 @@
+//#ifndef input_H
+//#define input_H
+
+
+#include <iostream>
+//#include <fstream>
+//#include <sstream>
+#include <string>
+#include <map>
+#include <cstdlib>
+#include "node.h"
+
+
+class Goto : public Node {
+	
+	public:
+	
+		Goto(int temp_Inst, std::string temp_Func, std::string temp_Data);
+		
+		void Execute(std::map<int, Node*> m, std::map<std::string, int> &v, std::map<int, Node*>::iterator &i);
+		
+	private:
+	
+	
+};
+
+//#endif
+
