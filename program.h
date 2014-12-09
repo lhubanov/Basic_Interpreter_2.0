312a428b2425353bcf484088098ff4c5afd9382e
yazoo123 <yazoo_123@hotmail.com>
2014-12-09 18:35:19 +0000

    Create program.h

---
+++ program.h
@@ -0,0 +1,53 @@
+//#ifndef DATABASE_H_
+//#define DATABASE_H_
+
+//contains declarations
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <cstdlib>
+
+#include "node.h"
+#include "print.h"
+
+
+class Node;
+
+class Program {
+	
+	private:
+	
+	//int ThisCounter;
+	
+	//testing starts here
+	//int FileIndex;
+
+	//testing ends here
+
+	public:
+	
+	//constructors and destructors
+	Program(); //default constructor; make private, when the custom one is finished
+	
+	~Program(); //destructor
+
+	//operations
+	//void sort_file_to_map(std::ifstream& other); //maybe redundant, considering we have read_file
+	//void evaluate_command(); //should take string command? discuss with others
+		
+	static std::map < int, Node* > program_map;
+	static std::map < std::string, int > variable_map;
+
+
+	friend std::istream& operator>> (std::istream& in, Program& p);
+	
+	void Run();
+	
+	void Print_map();
+};
+
+//#endif
+
+
