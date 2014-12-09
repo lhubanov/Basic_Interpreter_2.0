b2f2df2b34aac2f72c4ef1e6912148d31a9a57c3
yazoo123 <yazoo_123@hotmail.com>
2014-12-09 21:53:00 +0000

    Update print.cc

--- print.cc
+++ print.cc
@@ -5,10 +5,17 @@ Print::Print(int temp_Inst, std::string temp_Func, std::string temp_Data):Node(t
 	std::cout << "Print Constructor- inst, func, data are: " << temp_Inst << temp_Func << temp_Data << std::endl;	
 }
 
-void Print::Execute(std::map<int, Node*> m, std::map<std::string, int> v)
+void Print::Execute(std::map<int, Node*> m, std::map<std::string, int> &v, std::map<int, Node*>::iterator &i)
 {
 	std::cout << "Print Execute called" << std::endl;		
-	
-	std::cout << Data << std::endl; 
-
+		
+	if(v.find(Data) != v.end())
+	{
+		std::cout << v[Data] << std::endl;
+	}
+		
+	else
+	{
+		std::cout << Data << std::endl; 
+	}
 }
