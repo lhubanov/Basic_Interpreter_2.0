a9edfd7957ae71635d8da053017da8324d9bc2b1
yazoo123 <yazoo_123@hotmail.com>
2014-12-09 18:35:48 +0000

    Create node.h

---
+++ node.h
@@ -0,0 +1,29 @@
+//includes?
+#ifndef NODE_H_
+#define NODE_H_
+
+#include <iostream>
+//#include <fstream>
+//#include <sstream>
+#include <string>
+#include <map>
+#include <cstdlib>
+
+class Node {
+	
+	public:
+		
+		Node(int i, std::string f, std::string d):Instruction(i), Function(f), Data(d){};
+		
+		virtual void Execute(std::map<int, Node*> m, std::map<std::string, int> v) = 0;
+		
+	
+	protected: 
+	
+		int Instruction;
+		std::string Function;
+		std::string Data;	
+			
+};
+
+#endif
